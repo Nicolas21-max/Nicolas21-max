70dc1b53542096f65546e25751b2ae2befa98e17
Nicolas21-max <00001116076597SP@al.educacao.sp.gov.br>
2024-06-11 15:54:25 -0300

    adicionando arquivos

--- sketch.js
+++ sketch.js
@@ -1,1 +1,18 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 20;
+
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background("green");
+  circle(xBolinha, yBolinha, diametro);
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
 
